d40c503866e914a84d0851cb699cac8d328bb653
sebastiangus <sg222pm@student.lnu.se>
2016-11-22 15:07:49 +0100

    RED

--- assignment1/test/test.js
+++ assignment1/test/test.js
@@ -2,15 +2,21 @@
 var chai = require('chai');
 var SpreadsheetFactory = require('../model/SpreadsheetFactory'); // Require the System to test
 var Spreadsheet = require('../model/Spreadsheet'); // Require the System to test
+var Cell = require('../model/Cell');
 
 var expect = chai.expect;
 var assert = chai.assert;
 var should = chai.should();
 
 describe('factoryTest', function(){
+    var factory = new SpreadsheetFactory();
+
     it('spreadSheet function should return object constructed from Spreadsheet constructor', function () {
-        var factory = new SpreadsheetFactory();
-        var sut = factory.spreadsheet();
+        let sut = factory.spreadsheet();
         sut.constructor.should.equal(new Spreadsheet().constructor);
+    });
+
+    it('cell function should return object constructed from Spreadsheet constructor', function () {
+        let sut = factory.cell();
     });
 });
