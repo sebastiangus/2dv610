e8f46cf45b267b458f84017ccd5518c8bbc4222d
sebastiangus <sg222pm@student.lnu.se>
2016-11-22 15:09:18 +0100

    GREEN

--- assignment1/model/Cell.js
+++ assignment1/model/Cell.js
@@ -1,3 +1,10 @@
 /**
  * Created by sebastiangustavsson on 2016-11-22.
  */
+'use strict';
+
+function Cell() {
+
+}
+
+module.exports = Cell;
--- assignment1/model/SpreadsheetFactory.js
+++ assignment1/model/SpreadsheetFactory.js
@@ -2,12 +2,17 @@
  * Created by sebastiangustavsson on 2016-11-22.
  */
 var Spreadsheet = require('../model/Spreadsheet');
+var Cell = require('../model/Cell');
 
 function SpreadsheetFactory(){
 }
 
 SpreadsheetFactory.prototype.spreadsheet = function () {
     return new Spreadsheet();
+};
+
+SpreadsheetFactory.prototype.cell = function () {
+    return new Cell();
 };
 
 module.exports = SpreadsheetFactory;
