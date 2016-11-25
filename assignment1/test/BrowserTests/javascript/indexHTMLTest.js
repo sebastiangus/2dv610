3d52e6300a958e9db0564d80ce0633ade5e15c75
sebastiangus <sg222pm@student.lnu.se>
2016-11-25 14:50:57 +0100

    RED

--- assignment1/test/BrowserTests/javascript/indexHTMLTest.js
+++ assignment1/test/BrowserTests/javascript/indexHTMLTest.js
@@ -4,13 +4,21 @@ var expect = chai.expect;
 var should = chai.should();
 chai.use(require('chai-dom'));
 
-describe('DOM tests', function () {
+describe('cell-template tests', function () {
 
-    it('Class-test', function () {
+    it('html test', function () {
         //use import to get html-document
         var importedHtml = document.querySelector('link[rel=import]');
         var content = importedHtml.import;
         content.querySelector('#cell-template').should.have.html('<div class="cell"></div>');
+    });
+});
+
+describe('row-template tests', function () {
+    it('html test', function () {
+        var importedHtml = document.querySelector('link[rel=import]');
+        var content = importedHtml.import;
+        content.querySelector('#row-template').should.have.html('<div class="row"></div>');
     });
 });
 
