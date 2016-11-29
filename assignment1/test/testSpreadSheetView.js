232c507512ad28cf4ec51fb554991dd9f9328b18
sebastiangus <sg222pm@student.lnu.se>
2016-11-29 08:47:16 +0100

    GREEN

--- assignment1/test/testSpreadSheetView.js
+++ assignment1/test/testSpreadSheetView.js
@@ -1,13 +1,14 @@
-/**
- * Created by sebastiangustavsson on 2016-11-23.
- */
+'use strict';
 var chai =require('chai');
-var spreadFactory = require('.././model/SpreadsheetFactory');
+var SpreadSheet = require('.././model/Spreadsheet');
 var SpreadSheetView = require('.././view/SpreadSheetView');
+var Row = require('../model/Row');
 
 var expect = chai.expect;
 var assert = chai.assert;
 var should = chai.should();
+var spread = new SpreadSheet();
+var row = new Row();
 
 //How to check for thrown errors
 //http://stackoverflow.com/questions/14966821/testing-for-errors-thrown-in-mocha
@@ -17,7 +18,15 @@ describe('view Tests',function () {
        assert.Throw(SpreadSheetView, Error);
    });
 
+    it('view should throw error if initiated with Row',function () {
+      expect(function () {
+          var view = new SpreadSheetView(row);
+      }).to.throw(Error);
+    });
+
     it('view should not throw error if initiated with SpreadSheet', function () {
-       expect(SpreadSheetView).to.not.throw(Error);
-    })
+        expect(function () {
+            var view = new SpreadSheetView(spread);
+        }).to.not.throw(Error);
+    });
 });
--- assignment1/view/SpreadSheetView.js
+++ assignment1/view/SpreadSheetView.js
@@ -1,5 +1,9 @@
-function SpreadSheetView() {
-    throw new Error("SpreadSheetView can only be instantiated by passing reference to SpreadSheet");
+var Spreadsheet = require('../model/Spreadsheet');
+
+function SpreadSheetView(spreadsheet) {
+    if(spreadsheet.constructor !== Spreadsheet) {
+        throw new Error("SpreadSheetView can only be instantiated by passing reference to SpreadSheet");
+    }
 }
 
 module.exports = SpreadSheetView;
